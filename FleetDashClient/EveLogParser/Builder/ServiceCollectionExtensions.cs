1377dd6df3f0d8683e34c9e582639b59ae19db6d
Jamis Hartley <jamman98@gmail.com>
2022-01-24 22:02:53 -0700

    WIP: Add extension methods to configure parser

--- FleetDashClient/EveLogParser/Builder/ServiceCollectionExtensions.cs
+++ FleetDashClient/EveLogParser/Builder/ServiceCollectionExtensions.cs
@@ -1,3 +1,4 @@
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace EveLogParser.Builder;
@@ -6,8 +7,16 @@ public static class ServiceCollectionExtensions
 {
     public static IServiceCollection AddEveLogParser(this IServiceCollection services, Action<EveLogParserOptions> options)
     {
+        if (options == null) throw new ArgumentNullException(nameof(options));
         var builder = new EveLogParserOptions();
         options(builder);
+        return services;
+    }
+    
+    public static IServiceCollection AddEveLogParser(this IServiceCollection services, IConfiguration config)
+    {
+        if (config == null) throw new ArgumentNullException(nameof(config));
+        services.Configure<EveLogParserOptions>(config.Bind);
         return services;
     }
 }
