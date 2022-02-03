a547977a1c041030026377e7b8104eddd2a9324c
Sheemap <jamman98@gmail.com>
2022-02-02 22:38:14 -0700

    Connect to websocket with ticket

--- src/components/TheEventStream.vue
+++ src/components/TheEventStream.vue
@@ -1,29 +1,29 @@
 <script setup>
   import { useUserStore } from "../js/userStore";
   import { useEventStore } from "../js/eventStore";
-  import {ref} from "vue";
 
   const userStore = useUserStore();
   const eventStore = useEventStore();
 
-  let active = ref(false);
   userStore.getActiveToken().then(tokenSet => {
     eventStore.getStreamTicket(tokenSet).then(ticket => {
-      let ws = new WebSocket(`ws://${import.meta.env.VITE_FLEETDASH_CORE_URL}/api/event-stream?ticket=${ticket}`);
+      const ws = new WebSocket(`ws://${import.meta.env.VITE_FLEETDASH_CORE_URL}/api/event-stream?ticket=${ticket}`);
 
       ws.addEventListener('open', function open() {
         console.log('Connection has been established.');
-        active.value = true;
-
-        ws.send("Hey friend!")
-        ws.send("How it be? :)")
     });
 
+      ws.addEventListener('message', function incoming(data) {
+        console.log(data.data);
+      });
 
+      ws.addEventListener('close', function close() {
+        console.log('Connection has been closed.');
+      });
     });
   });
 </script>
 
 <template>
-{{active}}
+Streaming :)
 </template>
