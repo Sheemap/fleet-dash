a2a585d2f6f13a6d84746a59f55357e90735e577
Sheemap <jamman98@gmail.com>
2022-02-10 14:40:28 -0700

    Auto reconnect on socket timeout

--- src/components/TheEventStream.vue
+++ src/components/TheEventStream.vue
@@ -8,39 +8,44 @@
   const eventStore = useEventStore();
 
 
-  userStore.getActiveToken().then(tokenSet => {
-    eventStore.getStreamTicket(tokenSet).then(ticket => {
-      const socketScheme = import.meta.env.VITE_FLEETDASH_CORE_SCHEME === 'https' ? 'wss' : 'ws';
-      const ws = new WebSocket(`${socketScheme}://${import.meta.env.VITE_FLEETDASH_CORE_URL}/api/event-stream?ticket=${ticket}`);
+  function tryStartStream(){
+    userStore.getActiveToken().then(tokenSet => {
+      eventStore.getStreamTicket(tokenSet).then(ticket => {
+        const socketScheme = import.meta.env.VITE_FLEETDASH_CORE_SCHEME === 'https' ? 'wss' : 'ws';
+        const ws = new WebSocket(`${socketScheme}://${import.meta.env.VITE_FLEETDASH_CORE_URL}/api/event-stream?ticket=${ticket}`);
 
-      ws.addEventListener('open', function open() {
-        console.log('Connection has been established.');
+        ws.addEventListener('open', function open() {
+          console.log('Connection has been established.');
 
-        function timeoutFunc() {
-          ws.send(JSON.stringify({
-            type: 'ping'
-          }));
+          function timeoutFunc() {
+            ws.send(JSON.stringify({
+              type: 'ping'
+            }));
 
-          if(ws.readyState === WebSocket.OPEN) {
-            setTimeout(timeoutFunc, 5000);
+            if(ws.readyState === WebSocket.OPEN) {
+              setTimeout(timeoutFunc, 5000);
+            }
           }
-        }
 
-        timeoutFunc();
-      });
+          timeoutFunc();
+        });
 
-      ws.addEventListener('message', function incoming(data) {
-        let jsonData = JSON.parse(data.data)
-        console.log(data.data);
-        emitter.emit(jsonData.Type, jsonData);
-      });
+        ws.addEventListener('message', function incoming(data) {
+          let jsonData = JSON.parse(data.data)
+          console.log(data.data);
+          emitter.emit(jsonData.Type, jsonData);
+        });
 
-      ws.addEventListener('close', function close(event) {
-        console.log('Connection has been closed.');
-        console.log(event);
+        ws.addEventListener('close', function close(event) {
+          console.log('Connection has been closed.');
+          console.log(event);
+          tryStartStream();
+        });
       });
     });
-  });
+  }
+
+  tryStartStream();
 </script>
 
 <template>
