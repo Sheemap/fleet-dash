f0474b8a7d941b946ba8f645af74f5c5639273bd
Sheemap <jamman98@gmail.com>
2022-02-13 11:08:56 -0700

    Start session if one is not already running
    Also connect automatically if session exists

--- src/components/TheEventStream.vue
+++ src/components/TheEventStream.vue
@@ -1,5 +1,5 @@
 <script setup>
-  import { inject } from "vue";
+  import { inject, ref } from "vue";
   import { useUserStore } from "../js/userStore";
   import { useEventStore } from "../js/eventStore";
 
@@ -7,8 +7,11 @@
   const userStore = useUserStore();
   const eventStore = useEventStore();
 
+  let starting = ref(false);
 
-  function tryStartStream(){
+  function tryStartStream(startSession = true){
+    starting.value = true;
+
     userStore.getActiveToken().then(tokenSet => {
       eventStore.getStreamTicket(tokenSet).then(ticket => {
         const socketScheme = import.meta.env.VITE_FLEETDASH_CORE_SCHEME === 'https' ? 'wss' : 'ws';
@@ -43,6 +46,11 @@
           console.log('Connection has been closed.');
           console.log(event);
 
+          if (event.reason === "invalid ticket"){
+            eventStore.active = false;
+            eventStore.resetTicket();
+          }
+
           // If its been over a minute since last error, reset the counter
           if (eventStore.last_stream_failure_time < Date.now() - (1000 * 60)){
             eventStore.stream_failure_count = 0;
@@ -50,22 +58,24 @@
 
           // If no reason for close, we assume it's a request timeout, or some other retryable event
           // else its a permanent error
-          if(event.reason !== ""){
+          if(event.reason !== "" && event.reason !== "invalid ticket"){
             // TODO: dont use alert, use a snackbar
             alert('Connection closed: ' + event.reason);
             eventStore.active = false;
             eventStore.resetTicket();
+            starting.value = false;
           } else{
             // If we have tried to reconnect too many times, we dont want to keep trying
             if(eventStore.stream_failure_count < 5)
             {
               // Try to reconnect with an exponential backoff
-              setTimeout(tryStartStream, Math.pow(2, eventStore.stream_failure_count) * 1000);
+              setTimeout(tryStartStream, Math.pow(2, eventStore.stream_failure_count) * 1000, startSession);
             } else{
               // TODO: dont use alert, use a snackbar
               alert('We have tried to reconnect too many times. Please refresh the page to try again.');
               eventStore.active = false;
               eventStore.resetTicket();
+              starting.value = false;
             }
 
             // Increment the failure counter and set timer
@@ -73,14 +83,28 @@
             eventStore.last_stream_failure_time = Date.now();
           }
         });
-      });
+      })
+        .catch(err => {
+          if (startSession && err === 'not in active session') {
+            eventStore.startSession(tokenSet)
+                .then(() => {
+                  tryStartStream();
+                });
+          }else{
+            starting.value = false;
+          }
+        });
     });
   }
+
+  tryStartStream(false);
 </script>
 
 <template>
   <div v-if="!eventStore.active" class="grid place-content-center my-5">
-    <button class="bg-emerald-600 border border-emerald-800 py-2 px-3 rounded hover:bg-emerald-700 transition-all"
-            v-on:click="tryStartStream">Start Session</button>
+    <button v-if="starting" class="bg-emerald-600 border border-emerald-800 py-2 px-3 rounded hover:bg-emerald-700 transition-all"
+            v-on:click="tryStartStream">Join Session</button>
+    <button v-else class="bg-emerald-700 border border-emerald-800 py-2 px-3 rounded" disabled>
+      <svg class="animate-spin inline-block w-5 h-5 mb-1 mr-2 border-b-2 rounded-full" viewBox="0 0 24 24"></svg>Join Session</button>
   </div>
 </template>
--- src/js/eventStore.ts
+++ src/js/eventStore.ts
@@ -25,9 +25,13 @@ export const useEventStore = defineStore('event', {
                     fetch(`${import.meta.env.VITE_FLEETDASH_CORE_SCHEME}://${import.meta.env.VITE_FLEETDASH_CORE_URL}/api/event-stream/ticket`, options)
                         .then(response => response.json())
                         .then(response => {
-                            this._stream_ticket = response.ticket;
-                            localStorage.setItem('stream_ticket', this._stream_ticket);
-                            resolve(this._stream_ticket);
+                            if(typeof(response.error) === 'undefined') {
+                                this._stream_ticket = response.ticket;
+                                localStorage.setItem('stream_ticket', this._stream_ticket);
+                                resolve(this._stream_ticket);
+                            } else {
+                                reject(response.error);
+                            }
                         })
                         .catch(error => {
                             reject(error);
@@ -38,6 +42,21 @@ export const useEventStore = defineStore('event', {
         resetTicket() {
             this._stream_ticket = '';
             localStorage.removeItem('stream_ticket');
+        },
+        startSession(tokenSet : TokenSet) : Promise<void> {
+            const options = {
+                method: 'POST',
+                headers: {
+                    "Authorization": `Bearer ${tokenSet.access_token}`,
+                }
+            }
+            return fetch(`${import.meta.env.VITE_FLEETDASH_CORE_SCHEME}://${import.meta.env.VITE_FLEETDASH_CORE_URL}/api/session/start`, options)
+                .then(response => response.json())
+                .then(response => {
+                    if(typeof(response.error) !== 'undefined') {
+                        throw response.error;
+                    }
+                })
         },
     }
 });
