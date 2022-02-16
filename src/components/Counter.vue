8988f0b6d48470df86a95a8c60e32e85c669bc0f
Jamis Hartley <jamman98@gmail.com>
2022-02-16 16:48:03 -0700

    Add player ship card

--- src/components/Counter.vue
+++ src/components/Counter.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import {defineProps, computed} from 'vue';
 import numeral from 'numeral';
+import PlayerShipCardProgressCountdown from './PlayerShipCardProgressCountdown.vue'
 
 const props = defineProps<{
     title: string;
@@ -13,8 +14,10 @@ let readableCount = computed(() => numeral(props.count).format('0[.]00a'));
 </script>
 
 <template>
-    <div class="border rounded drop-shadow m-3 p-1 max-w-xs text-center bg-zinc-700">
+    <div class="border rounded drop-shadow m-3 p-2 max-w-xs text-center bg-zinc-800">
         <div class="text-2xl mt-4">{{title}}</div>
         <div class="text-6xl font-extrabold my-10" :class="[countColor]">{{readableCount}}</div>
+    <PlayerShipCardProgressCountdown player-name="Hello" ship-id="587" seconds="10" />
+
     </div>
 </template>
---
+++ src/components/PlayerShipCard.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+const props = defineProps<{
+    shipId: number;
+    playerName: string;
+    value: string;
+}>();
+
+const imgUrl = `https://images.evetech.net/types/${props.shipId}/icon`
+</script>
+
+<template>
+    <div class="p-1 bg-slate-600 w-full rounded">
+        <div class="w-full lg:max-w-full lg:flex">
+            <div class="h-12 w-12" ><img class="rounded" :src="imgUrl" /></div>
+            <div class="px-3">
+                <div>{{ playerName }}</div>
+                <div class="text-xs">{{ shipId }}</div>
+            </div>
+            <div class="p-3 flex-grow text-xl text-right">{{ value }}</div>
+        </div>
+        <slot></slot>
+    </div>
+</template>
