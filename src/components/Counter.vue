b9af9061f40b202f4dc5a65dbd001dfa53dce3c3
Sheemap <jamman98@gmail.com>
2022-04-01 22:57:26 -0600

    Change how we import numeral

--- src/components/Counter.vue
+++ src/components/Counter.vue
@@ -1,7 +1,6 @@
 <script setup lang="ts">
 import {computed} from 'vue';
-import * as numeral_ from 'numeral';
-const numeral = numeral_;
+import numeral from 'numeral';
 
 const props = defineProps<{
     title: string;
