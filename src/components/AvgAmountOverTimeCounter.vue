7677ebbfff95f25fc9da483991df4157fc64dfef
Sheemap <jamman98@gmail.com>
2022-04-04 16:59:11 -0600

    Fix issue with NaN on AvgAmountOverTimeCounter

--- src/components/AvgAmountOverTimeCounter.vue
+++ src/components/AvgAmountOverTimeCounter.vue
@@ -12,15 +12,19 @@ let runningAvg = ref(0);
 let count = ref(0);
 
 function removeValue(value){
-    runningAvg.value = ((runningAvg.value * count.value) - value) / (count.value - 1);
-    count.value--;    
+    if (count.value - 1  <= 0) {
+        runningAvg.value = 0;
+    } else {
+        runningAvg.value = ((runningAvg.value * count.value) - value) / (count.value - 1) || 0;
+    }
+    count.value--;
 }
 
 const emitter = inject("emitter");
 emitter.on(props.eventType, (evt) => {
     count.value++;
-    runningAvg.value = runningAvg.value + ((evt.Amount - runningAvg.value) / count.value);
-    setTimeout(removeValue, props.periodSeconds * 1000, evt.Amount);
+    runningAvg.value = runningAvg.value + ((evt.Amount - runningAvg.value) / count.value) || 0;
+  setTimeout(removeValue, props.periodSeconds * 1000, evt.Amount);
 });
 </script>
 
