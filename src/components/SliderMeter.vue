95abf1f6096f82ec88bc1fb0d4e3c65a8cd0d91e
Sheemap <jamman98@gmail.com>
2022-04-11 23:13:22 -0600

    Fix issue with NaN, and show split bar when theres no data

--- src/components/SliderMeter.vue
+++ src/components/SliderMeter.vue
@@ -11,11 +11,15 @@ const props = defineProps<{
 
 
 
-let leftPercent = computed(() => Math.round((props.leftValue / getTotal()) * 100));
-let rightPercent = computed(() => Math.round((props.rightValue / getTotal()) * 100));
+let leftPercent = computed(() => zeroIfNaN(Math.round((props.leftValue / getTotal()) * 100)));
+let rightPercent = computed(() => zeroIfNaN(Math.round((props.rightValue / getTotal()) * 100)));
 
 function getTotal() {
-    return parseFloat(props.leftValue) + parseFloat(props.rightValue);
+    return parseFloat(props.leftValue.toString()) + parseFloat(props.rightValue.toString());
+}
+
+function zeroIfNaN(value: number) {
+    return isNaN(value) ? 0 : value;
 }
 </script>
 
@@ -27,9 +31,13 @@ function getTotal() {
         <div class="h-7 w-1/2 rounded-l">{{ leftValue }}{{ textSuffix }}</div>
         <div class="h-7 w-1/2 rounded-r">{{ rightValue }}{{ textSuffix }}</div>
       </div>
-      <div class="flex pb-3 px-3">
+      <div v-if="rightPercent !== 0 || leftPercent !== 0" class="flex pb-3 px-3">
         <div class="bg-gradient-to-r from-emerald-400 to-emerald-600 h-7 rounded-l border-zinc-400" :class="leftPercent !== 100 || 'rounded'" :style="`width: ${leftPercent || 0}%;`"></div>
         <div class="bg-gradient-to-l from-red-500 to-red-600 h-7 w-1/2 rounded-r" :class="rightPercent !== 100 || 'rounded'" :style="`width: ${rightPercent || 0}%;`"></div>
+      </div>
+      <div v-else class="flex pb-3 px-3">
+        <div class="bg-gradient-to-r from-emerald-400 to-emerald-600 h-7 rounded-l border-zinc-400" style="width: 50%"></div>
+        <div class="bg-gradient-to-l from-red-500 to-red-600 h-7 w-1/2 rounded-r" style="width: 50%"></div>
       </div>
     </div>
 </template>
