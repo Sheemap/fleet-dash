5f45a2c057485ec4839a4384d9f89c27157ee74b
Sheemap <jamman98@gmail.com>
2022-04-17 15:01:54 -0600

    Make recent jams removable

--- src/components/RecentIncomingJams.vue
+++ src/components/RecentIncomingJams.vue
@@ -1,8 +1,11 @@
 <script setup lang="ts">
-import {computed, inject, reactive} from 'vue';
+import {computed, getCurrentInstance, inject, reactive} from 'vue';
 import PlayerShipCardProgressCountdown from './PlayerShipCardProgressCountdown.vue';
 import {useUserStore} from "../js/userStore";
 import {useFleetStore} from "../js/fleetStore";
+
+const keyStr = getCurrentInstance()?.vnode.key;
+const id = typeof keyStr === "string" ? keyStr.split("_")[1] : "";
 
 interface JammedTarget {
   name: string
@@ -62,10 +65,27 @@ function removeJam(key: JammedTargetKey) {
     jams.splice(jams.indexOf(jam), 1);
   });
 }
+
+function removeFromGrid(): void {
+  userStore.dash_layout = userStore.dash_layout.filter((item) => !item.i.endsWith(id));
+  localStorage.removeItem("widget_settings_" + id);
+}
 </script>
 
 <template>
   <div class="border rounded drop-shadow text-center bg-zinc-800 h-full overflow-auto">
+    <svg v-if="userStore.widget_drawer_open && id !== ''"
+         @click="removeFromGrid"
+         class="fixed z-50 top-0 left-0 m-1 config-button cursor-pointer transition ease-in-out opacity-70 hover:opacity-100"
+         xmlns="http://www.w3.org/2000/svg"
+         height="30px"
+         viewBox="0 0 24 24"
+         width="30px"
+         fill="#DC2626">
+      <path d="M0 0h24v24H0z" fill="none"/>
+      <path d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z"/>
+    </svg>
+
     <div class="text-2xl my-3">Incoming Jams</div>
 
     <div v-if="jams.length === 0" class="py-5 text-zinc-400 italic">No one currently jammed</div>
