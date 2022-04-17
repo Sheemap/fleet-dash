5ac844fbe63dcce5ede4aa4b2ea78c5896568c8e
Sheemap <jamman98@gmail.com>
2022-04-17 12:16:04 -0600

    Improve widget drawer styling

--- src/components/TheWidgetDrawer.vue
+++ src/components/TheWidgetDrawer.vue
@@ -9,6 +9,7 @@ import {createUUID} from "../js/shared";
 
 import {useUserStore} from "../js/userStore";
 import {INCOMING_DAMAGE_WIDGET_ID, SLIDER_WIDGET_ID, INCOMING_JAM_WIDGET_ID, OUTGOING_JAM_WIDGET_ID, OVERVIEW_WIDGET_ID} from "../js/constants";
+import AddOverlay from "./AddOverlay.vue";
 
 const props = defineProps<{
   dashboardRef: Ref;
@@ -34,23 +35,21 @@ function append(widgetId: string) {
 </script>
 
 <template>
-  <div class="flex flex-no-wrap bg-gray-200 h-full">
-    <div class="w-full absolute sm:relative bg-gray-800 shadow md:h-full grid grid-cols-1 gap-3">
-      <span class="mx-2 mt-2" @click="() => append(INCOMING_DAMAGE_WIDGET_ID)">
-        <Counter title="Counter" subtitle="" :count=0 />
-      </span>
-      <div class="mx-2 mt-2" @click="() => append(SLIDER_WIDGET_ID)">
-        <SliderMeter title="Sliding Meter" subtitle="" :left-value=0 :right-value=0 text-suffix="" />
-      </div>
-      <div class="mx-2 mt-2" @click="() => append(INCOMING_JAM_WIDGET_ID)">
-        <RecentIncomingJams />
-      </div>
-      <div class="mx-2 mt-2" @click="() => append(OUTGOING_JAM_WIDGET_ID)">
-        <RecentOutgoingJams />
-      </div>
-      <div class="mx-2 mt-2" @click="() => append(OVERVIEW_WIDGET_ID)">
-        <FleetOverview />
-      </div>
-    </div>
+  <div class="h-full px-3 py-5 rounded bg-opacity-80 bg-gray-800 grid grid-cols-1 gap-3">
+    <AddOverlay :widget-id="INCOMING_DAMAGE_WIDGET_ID" :append-func="append">
+      <Counter title="Counter" subtitle="" :count=0 />
+    </AddOverlay>
+    <AddOverlay :widget-id="SLIDER_WIDGET_ID" :append-func="append">
+      <SliderMeter title="Sliding Meter" subtitle="" :left-value=0 :right-value=0 text-suffix="" />
+    </AddOverlay>
+    <AddOverlay :widget-id="INCOMING_JAM_WIDGET_ID" :append-func="append">
+      <RecentIncomingJams />
+    </AddOverlay>
+    <AddOverlay :widget-id="OUTGOING_JAM_WIDGET_ID" :append-func="append">
+      <RecentOutgoingJams />
+    </AddOverlay>
+    <AddOverlay :widget-id="OVERVIEW_WIDGET_ID" :append-func="append">
+      <FleetOverview />
+    </AddOverlay>
   </div>
 </template>
--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -14,22 +14,29 @@ const dashboardRef = ref(null);
 </script>
 
 <template>
-  <div class="bg-zinc-900 text-slate-100 pb-96 md:pb-0 md:h-screen h-full">
+  <div class="bg-zinc-900 text-slate-100 pb-96 md:pb-0 h-screen h-full">
 
       <TheNavBar />
 
       <TheEventStream />
 
       <div v-if="eventStore.active">
-        <div id="content" class="flex gap-7 justi">
+        <div id="content" class="flex gap-7">
           <div class="w-full" :class="drawerOpened && 'w-3/4'">
             <TheDashboard :update-ref="(el) => dashboardRef = el" />
           </div>
-          <div v-if="drawerOpened" class="w-1/4">
+          <div v-if="drawerOpened" class="w-1/4 fixed right-0 top-0 h-full">
             <TheWidgetDrawer :dashboard-ref="dashboardRef" />
           </div>
-          <div class="absolute bottom-0 right-0 m-4 p-2 border rounded bg-slate-700 border-slate-500">
-            <button class="place-content-center" @click="() => drawerOpened = !drawerOpened">Add Widgets</button>
+          <div :class="drawerOpened ? 'opacity-100' : 'opacity-0'"
+               class="fixed top-0 right-0 m-4 p-1 rounded-full bg-red-500 cursor-pointer rotate-45 transition ease-in-out hover:scale-110"
+               @click="() => drawerOpened = !drawerOpened">
+            <img alt="Close Widget Drawer" src="https://img.icons8.com/material-outlined/36/000000/add.png"/>
+          </div>
+          <div :class="!drawerOpened ? 'opacity-100' : 'opacity-0'"
+               class="absolute bottom-0 right-0 m-6 p-1 rounded-full bg-emerald-500 cursor-pointer transition ease-in-out hover:scale-110"
+               @click="() => drawerOpened = !drawerOpened">
+            <img alt="Add Widget" src="https://img.icons8.com/material-outlined/30/000000/add.png"/>
           </div>
         </div>
       </div>
